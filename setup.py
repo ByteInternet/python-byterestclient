df82e00f175b2f4e34e1c053e92cd864866b2847
Maarten <maarten@informaarten.nl>
2014-02-20 10:28:15 +0100

    Fix typo in url

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='byterestclient',
     version='0.1',
     packages=find_packages(exclude=['test*']),
-    url='https:/github.com/ByteInternet/pythono-byterestclient',
+    url='https:/github.com/ByteInternet/python-byterestclient',
     author='Allard Hoeve',
     author_email='allard@byte.nl',
     description='A generic REST client',
