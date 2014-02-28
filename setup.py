3fc8c9ff55f7a6646b4d1e07b34aa6b5df0df378
Gertjan Oude Lohuis <gertjan@oudelohuis.nl>
2014-02-28 14:28:46 +0100

    allow setup.py to be run from any path

--- setup.py
+++ setup.py
@@ -1,4 +1,8 @@
+import os
 from setuptools import setup, find_packages
+
+# allow setup.py to be run from any path
+os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='byterestclient',
