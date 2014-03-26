ac5036e1f64e7a3a0b80d87f86c1693794314bfd
Allard Hoeve <allard@byte.nl>
2014-03-26 15:46:34 +0100

    Fix PyCharm introspection

--- setup.py
+++ setup.py
@@ -5,13 +5,13 @@ from setuptools import setup, find_packages
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
-    name='byterestclient',
+    name='python-byterestclient',
     version='0.1',
-    packages=find_packages(exclude=['test*']),
     url='https:/github.com/ByteInternet/python-byterestclient',
     author='Allard Hoeve',
     author_email='allard@byte.nl',
     description='A generic REST client',
     install_requires=['requests'],
     test_suite="tests",
+    packages=['byterestclient']
 )
