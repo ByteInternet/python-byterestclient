aa801988625381ec835a601ca37442aa829957de
Sophia Davis <sophia@byte.nl>
2015-05-19 10:50:22 +0200

    Add buildme

---
+++ buildme.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+set -e
+
+echo Updating changelog
+export version=$(date "+%Y%m%d.%H%M")
+git-dch --debian-tag="%(version)s" --new-version=$version --debian-branch master --release
+
+echo Updating version in setup.py
+sed -i -- "s/version='.*'/version='$version'/" setup.py
+
+echo Committing changelog and setup.py
+git add debian/changelog setup.py
+git commit -m "Annotate changelog, update version"
+
+echo Tagging and pushing tags
+git tag $version
+git push
+git push --tags
+
+echo Building package
+git-buildpackage --git-pbuilder --git-dist=wheezy --git-arch=amd64 --git-ignore-branch
+
+echo -e "Now to dput run:\ndput --unchecked -c /etc/byte.cf wheezy ../CHANGESFILE"
