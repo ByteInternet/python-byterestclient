2b64806c7e091a0a2f1e4a0e66b6788377e314eb
Sophia Davis <sophia@byte.nl>
2015-05-19 12:06:15 +0200

    Annotate changelog, update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='python-byterestclient',
-    version='20150519.1055',
+    version='20150519.1205',
     url='https:/github.com/ByteInternet/python-byterestclient',
     author='Allard Hoeve',
     author_email='allard@byte.nl',
