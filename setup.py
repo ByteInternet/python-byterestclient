78ffed7a1f5e85e3e878a922bd739f036aa1fe10
Allard Hoeve <allardhoeve@gmail.com>
2016-10-19 13:04:53 +0200

    Annotate changelog, update version

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='python-byterestclient',
-    version='20160728.1121',
+    version='20161019.1304',
     url='https://github.com/ByteInternet/python-byterestclient',
     author='Byte B.V.',
     author_email='tech@byte.nl',
